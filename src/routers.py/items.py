b1a4c8e3e6690859601d069165aa07d29646de72
SosisterRapStar <timoshenkovvanya@gmail.com>
2024-06-16 21:22:50 +0300

    added interface routing

--- src/routers.py/items.py
+++ src/routers.py/items.py
@@ -1,9 +1,26 @@
 from fastapi import APIRouter
 from starlette import status
-
+from typing import List
+import uuid
 router = APIRouter(tags=["Items"])
 
 
 
-@router.post("/create", status_code=status.HTTP_201_CREATED)
+@router.post("/create/", status_code=status.HTTP_201_CREATED)
+async def create_item():
+    pass
+
+
+@router.get("/{item_id}/", status_code=status.HTTP_200_OK)
+async def get_item(id: uuid.UUID):
+    pass
+
+@router.patch("/{item_id}/", status_code=status.HTTP_200_OK)
+async def update_item(item_id: uuid.UUID):
+    pass
+
+
+@router.get("/")
+async def get_items_by_categories(q: List[str]):
+    pass 
 
